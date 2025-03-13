b492029b211e73f72fd5962c96ca2affd9744d0f
muhsina <muhsinamolty44@gmail.com>
2025-03-13 09:54:26 +0530

    started node exporting and created git ignore

---
+++ index.js
@@ -0,0 +1,17 @@
+// const http = require("http");
+
+// const server =http.createServer(function(req,res) {
+//     res.write("hollo from http module ")
+//     res.end();
+// })
+// .listen("8080")
+
+const hello={
+    name:"sinu", 
+    place:"mongam",
+    greet: function(){
+        console.log(`i am ${this.name}`);
+        
+    }
+}
+module.exports=hello
---
+++ modules.js
@@ -0,0 +1,5 @@
+const hello=require("./index")
+
+console.log(hello.name);
+hello.greet()
+
