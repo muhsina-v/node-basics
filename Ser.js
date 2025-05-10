d957cb152d4f9f11ad422c1a82445f094ff5aea8
muhsina <muhsinamolty44@gmail.com>
2025-05-10 14:41:23 +0530

    updated

--- Ser.js
+++ Ser.js
@@ -1,11 +1,21 @@
-const http=require("http")
+// const http=require("http")
 
-const server=http.createServer( (req,res)=>{
-    res.writeHead(200,('condent-type:plain/text'))
-    res.end("endd")
+// const server=http.createServer( (req,res)=>{
+//     res.writeHead(200,('condent-type:plain/text'))
+//     res.end("endd")
 
-})
-server.listen(3000,()=>{
-    console.log("workinyyyy");
-    
-})
+// })
+// server.listen(3000,()=>{
+//     console.log("workinyyyy");
+
+// })
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writehead(200, { "content-type": "text/plain" });
+  res.end("hello node js ");
+});
+server.listen(3000, () => {
+  console.log("wwrrrrrrrrrrrrkkkkkkkkk");
+});
